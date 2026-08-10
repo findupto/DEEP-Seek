0237ee8d22c50c35d58ddad3342d4ece69e62834
Findupto <11663892+findupto@users.noreply.github.com>
2026-08-10 17:09:15 +0500

    Fix Windows batch escaping for MK app name

--- build_windows.bat
+++ build_windows.bat
@@ -2,7 +2,7 @@
 setlocal
 cd /d "%~dp0"
 
-echo === MK Pizza & Ice Bar - Windows EXE Build ===
+echo === MK Pizza ^& Ice Bar - Windows EXE Build ===
 python -m pip install -r requirements.txt
 if errorlevel 1 exit /b 1
 
